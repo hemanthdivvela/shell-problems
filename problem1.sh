d58779c9b37b7b0b40967d676f42b7053f8d036f
hemanthdivvela <divvelahemanth7@gmail.com>
2025-04-24 15:45:10 +0530

    problems

---
+++ problem1.sh
@@ -0,0 +1,13 @@
+useradd=$(id -u)
+timestamp=$(date+%F-%H-%M-%S)
+file-path=$(echo  $? | cut -d "." -f1)
+direction=$(file-path-timestamp.log)
+
+if [ useradd -ne 0 ]
+then
+    echo 'plese run this script with root sccess '
+
+else
+    echo 'you are super user'
+
+fi
