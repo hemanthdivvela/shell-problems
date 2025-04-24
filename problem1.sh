f3ff790a6d6e2ff44b808ae95f90133c9cb4db30
hemanthdivvela <divvelahemanth7@gmail.com>
2025-04-24 16:39:09 +0530

    problem

--- problem1.sh
+++ problem1.sh
@@ -4,16 +4,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-VALIDATA(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
 
-}
 if [ $USERID -ne 0 ]
 then
     echo "plese run this script with root sccess."
@@ -23,9 +14,4 @@ else
 fi
 
 dnf install mysql -y &>>$LOGFILE
-#$? is exit function
-VALIDATA $? "Installing MySQL"
 
-dnf install git -y &>>$LOGFILE
-#$? is exit function
-VALIDATA $? "Installing GIT"
