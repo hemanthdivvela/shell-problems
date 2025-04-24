1822a00fcc47e2a34d1a9f4756666dc42af70f81
hemanthdivvela <divvelahemanth7@gmail.com>
2025-04-24 16:28:25 +0530

    problems

--- problem1.sh
+++ problem1.sh
@@ -2,9 +2,9 @@
 useradd=$(id -u)
 timestamp=$(date +%F-%H-%M-%S)
 file-path=$(echo  $? | cut -d "." -f1)
-direction=$(file-path-timestamp.log)
+direction=$file-path-timestamp.log
 
-if [ useradd -ne 0 ]
+if [ $useradd -ne 0 ]
 then
     echo 'plese run this script with root sccess '
 
